754bf449e9ae3a9b13b0e7ddea3cdfbb56ea1c6b
Anton Nikulin <tsh@users.noreply.github.com>
2020-01-14 17:02:22 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,2 +1,8 @@
+# nvim
+# vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# emacs
 systemctl enable --user emacs
 systemctl start --user emacs
