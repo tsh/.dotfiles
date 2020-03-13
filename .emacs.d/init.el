7cb661971c0e31987995ede7670d0833eb3c6d8e
Anton Nikulin <tsh@users.noreply.github.com>
2020-03-13 17:18:03 +0200

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; add MELPA support
+;; add melpa
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
@@ -26,13 +26,13 @@ There are two things you can do about this warning:
   '(
     evil
     darcula-theme
+    el-get
     ))
 
 
 (tool-bar-mode -1)
 (toggle-scroll-bar -1) 
 (menu-bar-mode -1) 
-
 (set-frame-font "Source Code Pro-16")
 (global-visual-line-mode t)
 (delete-selection-mode t)
@@ -57,4 +57,49 @@ There are two things you can do about this warning:
 (require 'evil)
 (evil-mode 1)
 
+;; TRAMP
+;; Open files in Docker containers like so: /docker:drunk_bardeen:/etc/passwd
+(require 'tramp)
+(push
+ (cons
+  "docker"
+  '((tramp-login-program "docker")
+    (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+    (tramp-remote-shell "/bin/sh")
+    (tramp-remote-shell-args ("-i") ("-c"))))
+ tramp-methods)
+
+(defadvice tramp-completion-handle-file-name-all-completions
+  (around dotemacs-completion-docker activate)
+  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
+    a list of active Docker container names, followed by colons."
+  (if (equal (ad-get-arg 1) "/docker:")
+      (let* ((dockernames-raw (shell-command-to-string "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
+             (dockernames (cl-remove-if-not
+                           #'(lambda (dockerline) (string-match ":$" dockerline))
+                           (split-string dockernames-raw "\n"))))
+        (setq ad-return-value dockernames))
+    ad-do-it))
+
+
 (setq tramp-default-method "ssh")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (el-get yasnippet smooth-scroll smex neotree markdown-mode json-mode go-eldoc go-autocomplete git-gutter evil darcula-theme company-quickhelp company-go company-anaconda ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'ein)
+(require 'ein-notebook)
+(require 'ein-subpackages)
+
