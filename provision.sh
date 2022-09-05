ae5b2f2cd3f5e643956af3330aa66c3bd6b6b113
Anton Nikulin <tsh@users.noreply.github.com>
2022-09-05 21:11:13 +0300

    Update provision.sh
    install vim plugs

--- provision.sh
+++ provision.sh
@@ -2,6 +2,7 @@
 # vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim --headless +PlugInstall +qall
 
 # emacs
 systemctl enable --user emacs
