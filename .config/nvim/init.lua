4093349707cd1a6381b76c884c87abcdec3a897f
Anton Nikulin <tsh@users.noreply.github.com>
2022-10-06 21:44:40 +0300

    Update init.lua
    configure root_dir for python

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -146,24 +146,6 @@ local util = require('lspconfig/util')
 
 local path = util.path
 
-local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-  end
-
-  -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({'*', '.*'}) do
-    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-    if match ~= '' then
-      return path.join(path.dirname(match), 'bin', 'python')
-    end
-  end
-
-  -- Fallback to system Python.
-  return exepath('python3') or exepath('python') or 'python'
-end
-
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -204,9 +186,7 @@ vim.lsp.set_log_level("debug")
     flags = {
       debounce_text_changes = 150,
       },
-    --before_init = function(_, config)
-    --config.settings.python.pythonPath = get_python_path(config.root_dir)
-    --end
+	root_dir = nvim_lsp.util.root_pattern('.env')
   }
 
 
