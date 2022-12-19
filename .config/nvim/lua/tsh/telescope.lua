63a124ba0a23c93cc4c4fbf9bf9b066d028d5700
Anton Nikulin <dr.tallin@gmail.com>
2022-12-19 18:52:51 +0200

    add telescope

---
+++ .config/nvim/lua/tsh/telescope.lua
@@ -0,0 +1,21 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+
+telescope.setup {
+    defaults = {
+        color_devicons = false,
+        prompt_prefix = "$ ",
+        file_ignore_patterns = { "^.git/" },
+        mappings = {
+            i = {
+                ['<esc>'] = actions.close
+            }
+        }
+    }
+}
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
