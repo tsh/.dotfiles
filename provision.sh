ddffa46d4f9e79fad36c1518b23535803a9b2f6d
Anton Nikulin <tsh@users.noreply.github.com>
2022-12-19 19:44:20 +0200

    Update provision.sh
    add nix packages

--- provision.sh
+++ provision.sh
@@ -1,4 +1,18 @@
-# nvim
+# Install Nix
+sh <(curl -L https://nixos.org/nix/install) --daemon
+
+# Install Packages
+nix-env -iA \
+	nixpkgs.zsh \
+	nixpkgs.git \
+	nixpkgs.neovim \
+	nixpkgs.tmux \
+	nixpkgs.stow \
+	nixpkgs.fzf \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.direnv
+
 # vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
