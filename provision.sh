d7824d8eaed33c81f63842feae07cc7c97cf8bc6
Anton Nikulin <tsh@users.noreply.github.com>
2023-04-28 17:15:00 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,6 +1,11 @@
 # xclip to allow access to system clipboard from nvim on ubuntu 18
 sudo apt install xclip
 
+# install NerdFont for nvim. 
+# Note: Use this font in terminal for nvim to work
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
 sudo apt install snapd
 snap install nvim
 
