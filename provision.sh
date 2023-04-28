906aaf6ec1e8ea415feb7a8cc93a355720128076
Anton Nikulin <tsh@users.noreply.github.com>
2023-04-28 13:58:51 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,29 +1,11 @@
 # xclip to allow access to system clipboard from nvim on ubuntu 18
 sudo apt install xclip
 
-# === NIX ===
-# Install Nix
-sh <(curl -L https://nixos.org/nix/install) --daemon
-
-# Install Packages
-nix-env -iA \
-	nixpkgs.zsh \
-	nixpkgs.git \
-	nixpkgs.neovim \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.fzf \
-	nixpkgs.ripgrep \
-	nixpkgs.bat \
-	nixpkgs.direnv
-	
-# === END NIX ===
+sudo apt install snapd
+snap install nvim
 
 # vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim --headless +PlugInstall +qall
 
-# emacs
-systemctl enable --user emacs
-systemctl start --user emacs
