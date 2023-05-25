970869feb75c1e2443cc3160c31d4cfbc8ed4e8f
Anton Nikulin <tsh@users.noreply.github.com>
2023-05-25 16:55:50 +0300

    add ripgrep

--- provision.sh
+++ provision.sh
@@ -7,8 +7,10 @@ mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
 sudo apt install snapd
-snap install nvim
 
+# Neovim specific
+snap install nvim
+sudo apt-get install ripgrep
 # vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
