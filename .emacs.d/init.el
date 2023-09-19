8f2a1d47e0a9d71e4a69f7fd5e3e6eaaaeae5d11
Anton Nikulin <tsh@users.noreply.github.com>
2023-09-19 17:03:07 +0300

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,171 +1,56 @@
-;; add melpa
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(add-to-list 'package-archives  '("melpa" . "http://melpa.org/packages/"))
+ ;;(unless package-archive-contents  (package-refresh-contents))
 
-(package-initialize)
 
-(package-refresh-contents)
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(use-package lsp-mode
+  :hook
+  ((python-mode . lsp-deffered)))
 
-(defvar package-list
-  '(
-    evil
-    darcula-theme
-    el-get
-    projectile
-    neotree
-    git-gutter
+(use-package lsp-ui
+  :commands lsp-ui-mode)
 
-    elpy
-    flycheck
-    ;;py-autopep8
-    blacken
-    ein
-    ))
+(use-package lsp-pylsp
+  :ensure t)
 
+;; Load a custom theme
+;;(load-theme 'wombat t)
+(load-theme 'whiteboard t)
+;;; Vim Emulation
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+(require 'evil)
+(evil-mode 1)
+
+(unless (package-installed-p 'neotree)
+  (package-install 'neotree))
 
 (tool-bar-mode -1)
 (toggle-scroll-bar -1) 
 (menu-bar-mode -1) 
-(set-frame-font "Source Code Pro-16")
+(set-frame-font "Source Code Pro-14")
 (global-visual-line-mode t)
 (delete-selection-mode t)
 (show-paren-mode t)
-(global-linum-mode t)
-(setq inhibit-startup-screen t)
+;(global-linum-mode t)
+(global-display-line-numbers-mode 1)
+(save-place-mode 1)
 
 (ido-mode 1)
 (ido-everywhere 1)
 
-
-;; lock file, auto-save and auto-backup
-(setq backup-directory-alist
-     `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-     `((".*" ,temporary-file-directory t)))
-(setq create-lockfiles nil)
-
-(load-theme 'darcula t)
-
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
-
-;; TRAMP
-;; Open files in Docker containers like so: /docker:drunk_bardeen:/etc/passwd
-(require 'tramp)
-(push
- (cons
-  "docker"
-  '((tramp-login-program "docker")
-    (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
-    (tramp-remote-shell "/bin/sh")
-    (tramp-remote-shell-args ("-i") ("-c"))))
- tramp-methods)
-
-(defadvice tramp-completion-handle-file-name-all-completions
-  (around dotemacs-completion-docker activate)
-  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
-    a list of active Docker container names, followed by colons."
-  (if (equal (ad-get-arg 1) "/docker:")
-      (let* ((dockernames-raw (shell-command-to-string "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
-             (dockernames (cl-remove-if-not
-                           #'(lambda (dockerline) (string-match ":$" dockerline))
-                           (split-string dockernames-raw "\n"))))
-        (setq ad-return-value dockernames))
-    ad-do-it))
-
-
-(setq tramp-default-method "ssh")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (projectile el-get yasnippet smooth-scroll smex neotree markdown-mode json-mode go-eldoc go-autocomplete git-gutter evil darcula-theme company-quickhelp company-go company-anaconda ace-jump-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(require 'ein)
-(require 'ein-notebook)
-(require 'ein-subpackages)
-
-
-;; PYTHON DEV
-(elpy-enable)
-
-;; Use IPython for REPL
-;;(setq python-shell-interpreter "jupyter"
-;;      python-shell-interpreter-args "console --simple-prompt"
-;;      python-shell-prompt-detect-failure-warning nil)
-;;(add-to-list 'python-shell-completion-native-disabled-interpreters
-;;            "jupyter")
-
-
-;; Enable Flycheck
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-;; Projectile
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
+(setq inhibit-startup-screen t)
 ;; Neotree
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 (setq neo-theme 'arrow)
 
-(setq projectile-switch-project-action 'neotree-projectile-action)
-(setq neo-smart-open t)
+;; Store automatic customisation options elsewhere
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-(evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
-(evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
-(evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
-(evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
-(evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
-(evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
-(evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
-(evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
-(evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
-
-(add-hook 'neo-after-create-hook
-   #'(lambda (_)
-       (with-current-buffer (get-buffer neo-buffer-name)
-         (setq truncate-lines t)
-         (setq word-wrap nil)
-         (make-local-variable 'auto-hscroll-mode)
-         (setq auto-hscroll-mode t))))
-
-;; GIT GUTTER
-(global-git-gutter-mode t)
-(global-set-key (kbd "C-x C-g") 'git-gutter)
-(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
-;; Revert current hunk
-(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
-
-
-(add-to-list 'git-gutter:update-hooks 'focus-in-hook)
-(add-to-list 'git-gutter:update-commands 'other-window)
+;; lock file, auto-save and auto-backup
+(setq backup-directory-alist `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+      create-lockfiles nil)
