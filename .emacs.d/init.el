4b44b205318f833318fc97ab75fb401062058620
Anton Nikulin <tsh@users.noreply.github.com>
2025-02-12 15:18:06 +0200

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,11 @@
 
 (require 'package)
 (add-to-list 'package-archives  '("melpa" . "http://melpa.org/packages/"))
-;; (unless package-archive-contents  (package-refresh-contents))
+;;  (unless package-archive-contents  (package-refresh-contents))
 
 (package-initialize)
 
+;; AUTOCOMPLETE
 (use-package company
   :ensure t
   :config
@@ -14,19 +15,67 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; company-docs
+(require 'company-box)
+(add-hook 'company-mode-hook 'company-box-mode)
 
 
-(use-package python
-  :bind (:map python-ts-mode-map
-              ("<f5>" . recompile)
-              ("<f6>" . eglot-format))
-  :hook ((python-ts-mode . eglot-ensure)
-         (python-ts-mode . company-mode))
-  :mode (("\\.py\\'" . python-ts-mode)))
+;; LSP
+(add-hook 'python-mode-hook 'eglot-ensure)
 
+
+;; DAP
+(use-package dape
+  :preface
+  ;; By default dape shares the same keybinding prefix as `gud'
+  ;; If you do not want to use any prefix, set it to nil.
+  ;; (setq dape-key-prefix "\C-x\C-a")
+
+  :hook
+  ;; Save breakpoints on quit
+   (kill-emacs . dape-breakpoint-save)
+  ;; Load breakpoints on startup
+   (after-init . dape-breakpoint-load)
+
+  :config
+  ;; Turn on global bindings for setting breakpoints with mouse
+   (dape-breakpoint-global-mode)
+
+  ;; Info buffers to the right
+   (setq dape-buffer-window-arrangement 'right)
+
+  ;; Info buffers like gud (gdb-mi)
+  ;; (setq dape-buffer-window-arrangement 'gud)
+   (setq dape-info-hide-mode-line nil)
+
+  ;; Pulse source line (performance hit)
+   (add-hook 'dape-display-source-hook 'pulse-momentary-highlight-one-line)
+
+  ;; Showing inlay hints
+   (setq dape-inlay-hints t)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  ;; (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
+
+  ;; Kill compile buffer on build success
+  ;; (add-hook 'dape-compile-hook 'kill-buffer)
+
+  ;; Projectile users
+  ;; (setq dape-cwd-function 'projectile-project-root)
+  )
+
+;; Enable repeat mode for more ergonomic `dape' use
+(use-package repeat
+  :config
+  (repeat-mode))
+
+
+
+;; Virtual env
 (use-package pyvenv
   :config
   (pyvenv-mode 1))
+
 
 
 ;; Vim Emulation
