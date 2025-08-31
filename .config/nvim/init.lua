2c60096263e1b45f3b29ffb8d9010b5594111c2f
Anton Nikulin <dr.tallin@gmail.com>
2025-08-31 19:34:18 +0700

    fix errors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,10 @@
 local Plug = vim.fn['plug#']
 
+-- let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+--if empty(glob(data_dir . '/autoload/plug.vim'))
+--  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+--  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+
 vim.call('plug#begin', '~/.config/nvim/plugged')
 -- DEV
 Plug 'neovim/nvim-lspconfig'
--- .config/nvim/lua/tsh/init.lua
+++ .config/nvim/lua/tsh/init.lua
@@ -1,4 +1,3 @@
---require ('tsh/comment')
 require ('tsh/dap')
 require ('tsh/ui')
 require ('tsh/ux')
