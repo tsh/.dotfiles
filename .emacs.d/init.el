dd2d440594e92713175fe3435100223540874608
Anton Nikulin <dr.tallin@gmail.com>
2026-03-01 09:13:23 +0700

    chng emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,15 @@
-
+;;
 (require 'package)
-(add-to-list 'package-archives  '("melpa" . "http://melpa.org/packages/"))
-;;  (unless package-archive-contents  (package-refresh-contents))
+(add-to-list 'package-archives  '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
+
+
+(when (not package-archive-contents)
+    (package-refresh-contents))
+
+
+(require 'use-package)
 
 ;; AUTOCOMPLETE
 (use-package company
@@ -13,114 +19,151 @@
         company-minimum-prefix-length 1)
   )
 
-(add-hook 'after-init-hook 'global-company-mode)
-
-;; company-docs
-(require 'company-box)
-(add-hook 'company-mode-hook 'company-box-mode)
-
-
-;; LSP
-(add-hook 'python-mode-hook 'eglot-ensure)
-
-
-;; DAP
-(use-package dape
-  :preface
-  ;; By default dape shares the same keybinding prefix as `gud'
-  ;; If you do not want to use any prefix, set it to nil.
-  ;; (setq dape-key-prefix "\C-x\C-a")
-
-  :hook
-  ;; Save breakpoints on quit
-   (kill-emacs . dape-breakpoint-save)
-  ;; Load breakpoints on startup
-   (after-init . dape-breakpoint-load)
-
-  :config
-  ;; Turn on global bindings for setting breakpoints with mouse
-   (dape-breakpoint-global-mode)
-
-  ;; Info buffers to the right
-   (setq dape-buffer-window-arrangement 'right)
-
-  ;; Info buffers like gud (gdb-mi)
-  ;; (setq dape-buffer-window-arrangement 'gud)
-   (setq dape-info-hide-mode-line nil)
-
-  ;; Pulse source line (performance hit)
-   (add-hook 'dape-display-source-hook 'pulse-momentary-highlight-one-line)
-
-  ;; Showing inlay hints
-   (setq dape-inlay-hints t)
-
-  ;; Save buffers on startup, useful for interpreted languages
-  ;; (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
-
-  ;; Kill compile buffer on build success
-  ;; (add-hook 'dape-compile-hook 'kill-buffer)
-
-  ;; Projectile users
-  ;; (setq dape-cwd-function 'projectile-project-root)
+(use-package plantuml-mode
+  :ensure t
+  :config 
+	   (setq plantuml-jar-path "~/Downloads/plantuml-1.2026.1.jar")
+		(setq plantuml-default-exec-mode 'jar)
+		;; Enable plantuml-mode for PlantUML files
+		(add-to-list 'auto-mode-alist '("\\.puml\\'" . plantuml-mode))
+		;;( plantuml-download-jar)
   )
 
-;; Enable repeat mode for more ergonomic `dape' use
-(use-package repeat
-  :config
-  (repeat-mode))
+
+;; ORG
+(setq org-startup-with-inline-images t)
+(setq org-confirm-babel-evaluate nil)
 
 
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((plantuml . t))) ; this line activates plantuml
+(setq org-plantuml-jar-path (expand-file-name "~/Downloads/plantuml-1.2026.1.jar"))
 
-;; Virtual env
-(use-package pyvenv
-  :config
-  (pyvenv-mode 1))
+(add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
+(org-babel-do-load-languages 'org-babel-load-languages '((plantuml . t)))
 
 
-
+;;(add-hook 'after-init-hook 'global-company-mode)
+;;
+;;;; company-docs
+;;(require 'company-box)
+;;(add-hook 'company-mode-hook 'company-box-mode)
+;;
+;;
+;;;; LSP
+;;(add-hook 'python-mode-hook 'eglot-ensure)
+;;
+;;
+;;;; DAP
+;;(use-package dape
+;;  :preface
+;;  ;; By default dape shares the same keybinding prefix as `gud'
+;;  ;; If you do not want to use any prefix, set it to nil.
+;;  ;; (setq dape-key-prefix "\C-x\C-a")
+;;
+;;  :hook
+;;  ;; Save breakpoints on quit
+;;   (kill-emacs . dape-breakpoint-save)
+;;  ;; Load breakpoints on startup
+;;   (after-init . dape-breakpoint-load)
+;;
+;;  :config
+;;  ;; Turn on global bindings for setting breakpoints with mouse
+;;   (dape-breakpoint-global-mode)
+;;
+;;  ;; Info buffers to the right
+;;   (setq dape-buffer-window-arrangement 'right)
+;;
+;;  ;; Info buffers like gud (gdb-mi)
+;;  ;; (setq dape-buffer-window-arrangement 'gud)
+;;   (setq dape-info-hide-mode-line nil)
+;;
+;;  ;; Pulse source line (performance hit)
+;;   (add-hook 'dape-display-source-hook 'pulse-momentary-highlight-one-line)
+;;
+;;  ;; Showing inlay hints
+;;   (setq dape-inlay-hints t)
+;;
+;;  ;; Save buffers on startup, useful for interpreted languages
+;;  ;; (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
+;;
+;;  ;; Kill compile buffer on build success
+;;  ;; (add-hook 'dape-compile-hook 'kill-buffer)
+;;
+;;  ;; Projectile users
+;;  ;; (setq dape-cwd-function 'projectile-project-root)
+;;  )
+;;
+;;;; Enable repeat mode for more ergonomic `dape' use
+;;(use-package repeat
+;;  :config
+;;  (repeat-mode))
+;;
+;;
+;;
+;;;; Virtual env
+;;(use-package pyvenv
+;;  :config
+;;  (pyvenv-mode 1))
+;;
+;;
+;;
 ;; Vim Emulation
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-(require 'evil)
-(evil-mode 1)
+;;(unless (package-installed-p 'evil)
+;;  (package-install 'evil))
+;;(require 'evil)
+;;(evil-mode 1)
 
 (unless (package-installed-p 'neotree)
   (package-install 'neotree))
-
-(tool-bar-mode -1)
-(toggle-scroll-bar -1) 
-(menu-bar-mode -1) 
-(set-frame-font "Source Code Pro-14")
-(global-visual-line-mode t)
-(delete-selection-mode t)
-(show-paren-mode t)
-;(global-linum-mode t)
-(global-display-line-numbers-mode 1)
-(save-place-mode 1)
-
-(ido-mode 1)
-(ido-everywhere 1)
-
-(setq inhibit-startup-screen t)
-
-;; Neotree
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-(setq neo-theme 'arrow)
-
-;; Store automatic customisation options elsewhere
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file))
-
-;; lock file, auto-save and auto-backup
-(setq backup-directory-alist `((".*" . ,temporary-file-directory))
-      auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
-      create-lockfiles nil)
-
-
-;; Load a custom theme
-;;(load-theme 'wombat t)
- (load-theme 'whiteboard t)
-
-
+;;
+;;(tool-bar-mode -1)
+;;(toggle-scroll-bar -1) 
+;;(menu-bar-mode -1) 
+;;(set-frame-font "Source Code Pro-14")
+;;(global-visual-line-mode t)
+;;(delete-selection-mode t)
+;;(show-paren-mode t)
+;;;(global-linum-mode t)
+;;(global-display-line-numbers-mode 1)
+;;(save-place-mode 1)
+;;
+;;(ido-mode 1)
+;;(ido-everywhere 1)
+;;
+;;(setq inhibit-startup-screen t)
+;;
+;;;; Neotree
+;;(require 'neotree)
+;;(global-set-key [f8] 'neotree-toggle)
+;;(setq neo-theme 'arrow)
+;;
+;;;; Store automatic customisation options elsewhere
+;;(setq custom-file (locate-user-emacs-file "custom.el"))
+;;(when (file-exists-p custom-file)
+;;  (load custom-file))
+;;
+;;;; lock file, auto-save and auto-backup
+;;(setq backup-directory-alist `((".*" . ,temporary-file-directory))
+;;      auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+;;      create-lockfiles nil)
+;;
+;;
+;;;; Load a custom theme
+(load-theme 'wombat t)
+;;(load-theme 'whiteboard t)
+;;
+;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package neotree evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
